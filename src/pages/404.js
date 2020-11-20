10eb8b194af429607cadd6fe1b6ad2a336406729
Bohdan <bohdanimiela@gmail.com>
2020-11-20 10:01:25 +0100

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,16 @@ import React from "react"
 import Layout from "../layouts"
 import SEO from "../components/SEO"
 
+import styles from "./404.module.css"
+
 export default function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className={styles.container}>
+        <h1 className={styles.title}>NOT FOUND</h1>
+        <p className={styles.txt}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
     </Layout>
   )
 }
