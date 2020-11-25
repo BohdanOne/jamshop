697921582ad0977e0ad3d78ede4e435f9b21ae56
Bohdan <bohdanimiela@gmail.com>
2020-11-25 12:33:15 +0100

    CSS fixes

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,7 +3,6 @@ import { useStaticQuery, graphql, Link } from "gatsby"
 
 import HeroBackground from "../../assets/elements/hero_background.svg"
 import HeroImage from "../../assets/elements/monitor.svg"
-import HeroDecor from "../../assets/elements/hero_decor.svg"
 import styles from "./hero.module.css"
 
 function HomepageHero() {
@@ -11,20 +10,27 @@ function HomepageHero() {
   const { hero } = data.markdownRemark.frontmatter
 
   return (
-    <section className={styles.container}>
-      <img src={HeroImage} className={styles.heroImg} alt="Vector Monitor" />
-      <div className={styles.content}>
-        <img src={HeroDecor} className={styles.decorImg} />
-        <h1
-          className={styles.title}
-          dangerouslySetInnerHTML={{ __html: hero.title }}
-        />
-        <Link to={hero.cta.href} className={styles.btn}>
-          {hero.cta.text}
-        </Link>
-      </div>
+    <>
+      <section className={styles.container}>
+        <div className={styles.imgContainer}>
+          <img
+            src={HeroImage}
+            className={styles.heroImg}
+            alt="Vector Monitor"
+          />
+        </div>
+        <div className={styles.contentContainer}>
+          <h1
+            className={styles.title}
+            dangerouslySetInnerHTML={{ __html: hero.title }}
+          />
+          <Link to={hero.cta.href} className={styles.btn}>
+            {hero.cta.text}
+          </Link>
+        </div>
+      </section>
       <img src={HeroBackground} className={styles.backgroundImg} />
-    </section>
+    </>
   )
 }
 
