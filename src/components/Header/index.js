1727670c89968b6ff1cbc6507334cafee6a6e398
Bohdan <bohdanimiela@gmail.com>
2020-11-26 10:04:21 +0100

    Add background to header on scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Cart from "components/Cart"
 import styles from "./header.module.css"
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false)
+  const HEADER_HEIGHT = 60
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > HEADER_HEIGHT
+      if (isScrolled !== scrolled) {
+        setScrolled((scrolled) => !scrolled)
+      }
+    }
+    document.addEventListener("scroll", handleScroll)
+    return () => {
+      document.removeEventListener("scroll", handleScroll)
+    }
+  }, [scrolled])
+
   return (
-    <header className={styles.header}>
+    <header data-scrolled={scrolled} className={styles.header}>
       <div className={styles.container}>
         <div>
           <Link className={styles.title} to="/">
